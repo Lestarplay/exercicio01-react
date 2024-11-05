0ff88843fbac0fb77b2ab38535f63abf495e9f24
Breno B.franco <162745949+Lestarplay@users.noreply.github.com>
2024-11-04 21:24:36 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Saudacao from './assets/components/Saudacao'
+
+function App(){
+  return(
+    <div className='App'>
+       <Saudacao/>
+    </div>
+  )
+}
+
+
+
+export default App
---
+++ src/assets/components/Saudacao.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Saudacao(props){
+    return <h1>Olá,Breno! {props.name}</h1>
+}
+
+export default Saudacao
